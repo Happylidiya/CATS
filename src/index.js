52fd2abc7ae73348d19fece71c95597a13ec5261
Happylidiya <Happylidiya5@gmail.com>
2023-03-04 14:17:46 +0300

    Тест

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const cardsContainer = document.querySelector('.cards');
-const btnOpenPopup = document.querySelector('.btn');
+const btnOpenPopup = document.querySelector('#add');
+const btnOpenPopupLogin = document.querySelector('#login');
 const formCatAdd = document.querySelector('#popup-form-add')
+const formLogin = document.querySelector('#popup-form-login')
+const isAuth = Cookies.get("email");
 
 const popupAdd = new Popup('popup-add');
 const popupImage = new PopupWithImage('popup-cat-image');
-
+const popupLogin = new Popup('popup-login');
 
 function serializeForm(elements) {
     const formData = {};
@@ -27,31 +30,66 @@ function handleFormAddCat(e) {
     e.preventDefault();
     const elementsFormCat = [...formCatAdd.elements];
     const formData = serializeForm(elementsFormCat);
-    const newElement = new Card(formData, '#card-template', handleClickCatImage);
-    cardsContainer.prepend(newElement.getElement());
-
-    popupAdd.close();
+    api.addNewCat(formData)
+      .then(function() {
+        const newElement = new Card(formData, '#card-template', handleClickCatImage);
+        cardsContainer.prepend(newElement.getElement());
+        popupAdd.close();
+      })
+      .catch(function(err){
+        console.log(err);
+      })
 }
 
 function handleClickCatImage(dataSrc) {
     popupImage.open(dataSrc)
 }
 
-
+function handleFormLogin(e) {
+  e.preventDefault();
+  const elementsFormLogin = [...formLogin.elements];
+  const formData = serializeForm(elementsFormLogin);
+  Cookies.set("email", formData.email, {expires: 15});
+  btnOpenPopup.classList.remove('visually-hidden');
+  btnOpenPopupLogin.classList.add('visually-hidden');
+  popupLogin.close();
+}
 
 formCatAdd.addEventListener('submit', handleFormAddCat);
+formLogin.addEventListener('submit', handleFormLogin);
 
 btnOpenPopup.addEventListener('click', (e) => {
     e.preventDefault();
     popupAdd.open();
 });
 
-cats.forEach( catData => {
-    const newElement = new Card(catData, '#card-template', handleClickCatImage);
-    cardsContainer.append(newElement.getElement());
-
+btnOpenPopupLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  popupLogin.open();
 });
+
+
+api.getAllCats()
+   .then(data => {
+      data.forEach( catData => {
+        const newElement = new Card(catData, '#card-template', handleClickCatImage);
+        cardsContainer.prepend(newElement.getElement());
+      });
+   })
+   .catch(function(err){
+    console.log(err);
+  })
+
+
+if (!isAuth) {
+  popupLogin.open();
+  btnOpenPopup.classList.add('visually-hidden');
+} else {
+  btnOpenPopupLogin.classList.add('visually-hidden');
+}
+
 
 popupAdd.setEventListener();
 popupImage.setEventListener();
+popupLogin.setEventListener();
 
